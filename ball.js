e5c9dfdbde25c1c861288a0ba67d42c9981abc3b
Jose Armas <armasjose95@gmail.com>
2022-10-23 21:22:24 -0500

    adding js

---
+++ ball.js
@@ -0,0 +1,8 @@
+export default class Ball{  //naming class called ball
+    constructor(ballElem) {
+        this.ballElem = ballElem  //inside the constructor we're going to pass in the element that corresponds to our ball.
+    }
+    update(delta) {
+        
+    }
+}
---
+++ script.js
@@ -0,0 +1,40 @@
+/* Update Loop: Every single frame that passes, we're calling a function.
+-That function is going to update the positions and the logic of all the pieces of my game.
+-ex. Computer AI is going to move the paddle & ball is going to move in the current direction it's goin.
+-I can move my paddle wih my mouse.
+-This is going to update all the positions and paint everything on the screen exactly where it needs to go inside of the update loop.
+-Breaking out everything in my game into seperate classes.
+*/
+
+import Ball from "./Ball.js" //imported filed from ball.js
+
+/*selecting the ball HTML element and creating a new class for that ball & now have access to it here & can use inside 
+our update loop
+*/
+const ball = new Ball(document.getElementById("ball")) 
+
+let lastTime
+//update loop is going to take in a time variable for how much time has passed since the start of our program
+function update(time){ 
+    /* Every time we call update, we're taking our last time and we're subtracting our time from that last time
+    to get our delta and then we're setting our lastTime =(to) time 
+    Then it'll repeat but the 1st time we call update lastTime is null, we don't have a lasttime yet, need to check 4 that.
+    */
+
+    if (lastTime != null) {
+        const delta = time - lastTime
+        //Update Code happening here only if we have a last time. We just set our lastTime and call it again the 1st time.
+        console.log(delta)
+    }
+    lastTime = time
+    window.requestAnimationFrame(update)
+} 
+
+/*In order to call update(time) function
+-With requestAnimationFrame is every time that you can change what's on the screen, this function is going to be called.
+-JS knows you can't change anything on the screen, so  don't run code.
+-But as soon as you can run something on the screen, it calls this function.
+-As long as we call window.requestAnimationFrame, it's going to infinitley loop.
+-every frame it calls this function
+*/
+window.requestAnimationFrame(update)
